75eb4e0df3018636077b48f3ae48b40ad47862f4
Gabby Broussard <gabriella.b.broussard@gmail.com>
2020-10-07 12:30:06 -0500

    removing line 29

--- acquire_kwame.py
+++ acquire_kwame.py
@@ -26,7 +26,6 @@ def get_zillow_data():
         # read the SQL query into a dataframe
         df = pd.read_sql(sql_query, get_connection('zillow'))
         #drop second index column
-        df = df.drop(columns=['Unnamed: 0'])
         # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_file(filename)
         # Return the dataframe to the calling code
